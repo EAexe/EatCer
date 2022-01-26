aaefd3c53fb93eed9f25812defa9c6ff7b917b65
饿啊.exe <98443063+EAexe@users.noreply.github.com>
2022-01-26 14:21:04 +0800

    Update index.js

--- static/index.js
+++ static/index.js
@@ -347,14 +347,14 @@
         let date2 = new Date();
         deviation_time = (date2.getTime() - _date1.getTime())
         if (deviation_time > 23000) {
-            return '倒计时多了' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
+            return '啊呜，开车时间是不是太久了(。G ∩ G)' + ((deviation_time / 1000) - 20).toFixed(2) + "s";
         }
         SubmitResults();
-        if (score <= 49) return '试着好好练一下？';
-        if (score <= 99) return 'TCL';
-        if (score <= 149) return 'TQL';
-        if (score <= 199) return '您';
-        return '人？';
+        if (score <= 49) return '不合格(。G ∩ G)';
+        if (score <= 99) return 'bad bad 2 me OH YEAH!';
+        if (score <= 149) return '合格(。G ∪ G)';
+        if (score <= 199) return '您！';
+        return '您您您您';
     }
 
     w.toStr = function(obj) {
